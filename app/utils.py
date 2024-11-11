85565895c0e763ee04516a477f936f3250471c4b
abdurrahman faris rizqullah <nugasriz03@gmail.com>
2024-11-11 10:07:19 +0700

    fix: fix error deploy training model on fastAPI

--- app/utils.py
+++ app/utils.py
@@ -1,26 +1,41 @@
 import joblib
 from typing import Dict
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Load the models from the .pkl file
 models: Dict[str, object] = joblib.load('app/ml/models.pkl')
 
-# Check if 'model' exists in the loaded models
-if 'model' not in models:
-    raise KeyError("'model' not found in models.pkl")
+# Check if both models exist in the loaded models
+if 'energy_model' not in models or 'comfort_model' not in models:
+    raise KeyError("'energy_model' or 'comfort_model' not found in models.pkl")
 
-model = models['model']  # Assuming you have a model key
+energy_model = models['energy_model']
+comfort_model = models['comfort_model']
+
+class InputData(BaseModel):
+    people: int
+    temperature: float
+    humidity: float
+    lightIntensity: int
+    noise: int
+    co2: int
+    pm25: float
+    airflow: float
 
 def update_with_ml_model(data: dict) -> dict:
     # Extract features from data
-    features = [data['temperature'], data['humidity'], data['light_intensity'],
+    features = [data['people'], data['temperature'], data['humidity'], data['lightIntensity'],
                 data['noise'], data['co2'], data['pm25'], data['airflow']]
 
     # Reshape features to match the model input
     reshaped_features = [features]
 
     # Predict the energy and comfort
-    predictions = model.predict(reshaped_features)
-    data['energy'] = predictions[0][0]
-    data['comfort'] = predictions[0][1]
-    
+    energy_prediction = energy_model.predict(reshaped_features)
+    comfort_prediction = comfort_model.predict(reshaped_features)
+
+    data['energy'] = energy_prediction[0]
+    data['comfort'] = comfort_prediction[0]
+
     return data
